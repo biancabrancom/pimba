4d8af750b35dd062a30ead618f38a54608b22c98
Bianca Branco <92753001+nckzi@users.noreply.github.com>
2022-10-31 19:27:08 -0300

    Update menu.c
    mds sou mt burra tava editando no arquivo antigo no pc q raivaaaaa
    tudo ok agora
    
    como apaga aqls diretorio veio mds

--- menu.c
+++ menu.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h> //biblioteca para uso de acentuação em pt-br
+#include <locale.h>
 
 int opcao, opcao_cad, opcao_rlt, opcao_crono, opcao_av;
 
 int main()
 {
-    setlocale(LC_ALL, ""); // chamando local
+    setlocale(LC_ALL, "");
 
     int continuar = 1;
 
     do
     {
         system("cls");
-        printf("PIM.invest\n");
-        printf("Bem vindo, investidor!\n\n");
-        printf("       Menu\n");
-        printf(" -----------------\n");
-        printf("|  1. Cadastro    |\n");
-        printf("|  2. Relatórios  |\n");
-        printf("|  3. Cronograma  |\n");
-        printf("|  4. Avisos      |\n");
-        printf(" -----------------\n\n");
+        printf("       InvistaC\n");
+        printf(" ----------------------\n");
+        printf(" Bem vindo, investidor!\n\n");
+        printf("         Menu\n");
+        printf("   -----------------\n");
+        printf("  |  1. Cadastro    |\n");
+        printf("  |  2. Relatorios  |\n");
+        printf("  |  3. Cronograma  |\n");
+        printf("  |  4. Avisos      |\n");
+        printf("   -----------------\n\n");
 
-        printf("Escolha a opção desejada.\n");
+        printf(" Escolha a opcao desejada.\n");
         scanf("%d", &opcao);
 
         switch (opcao)
@@ -55,18 +56,18 @@ int main()
 }
 
 int cadastro() {
-    system("cls");
-    printf("        Cadastro\n");
-    printf(" ------------------------\n");
-    printf("|  1. Área do cliente    |\n");
-    printf("|  2. Equipe PIM.invest  |\n");
-    printf("|  0. Voltar             |\n");
-    printf(" ------------------------\n\n");
+    printf("           Cadastro\n");
+    printf("  -----------------------------\n");
+    printf(" |  1. Cadastro de cliente     |\n");
+    printf(" |  2. Cadastro de funcionario |\n");
+    printf(" |  0. Voltar                  |\n");
+    printf("  -----------------------------\n\n");
 
-    printf("Escolha a opção desejada.\n");
+    printf(" Escolha a opcao desejada.\n");
     scanf("%d", &opcao_cad);
+}
 
-    switch(opcao_cad)
+    /*switch(opcao_cad)
     {
     case 1:
         system("cls");
@@ -75,54 +76,72 @@ int cadastro() {
         break;
         }
 
-}
-
 int area_cliente() {
     system("cls");
     printf("OIIIII");
-}
+}*/
 
 int relatorios() {
-    system("cls");
-    printf("   Relatórios\n");
-    printf(" ---------------\n");
-    printf("|  1. Clientes  |\n");
-    printf("|  2. Empresa   |\n");
-    printf("|  0. Voltar    |\n");
-    printf(" ---------------\n\n");
+    printf("    Relatorios\n");
+    printf("  ---------------\n");
+    printf(" |  1. Clientes  |\n");
+    printf(" |  2. Empresa   |\n");
+    printf(" |  0. Voltar    |\n");
+    printf("  ---------------\n\n");
 
-    printf("Escolha a opção desejada.\n");
+    printf(" Aviso: Os relatorios serao descarregados em formato .txt\n");
+    printf(" Escolha a opcao desejada.\n");
     scanf("%d", &opcao_rlt);
 }
 
 int cronograma() {
-    system("cls");
-    printf("   Cronograma\n");
-    printf(" ---------------\n");
-    printf("|  1. Semanal  |\n");
-    printf("|  2. Mensal   |\n");
-    printf("|  0. Voltar   |\n");
-    printf(" ---------------\n\n");
+    printf("    Cronograma\n");
+    printf("  ---------------\n");
+    printf(" |  1. Semanal  |\n");
+    printf(" |  2. Mensal   |\n");
+    printf(" |  0. Voltar   |\n");
+    printf("  ---------------\n\n");
 
-    printf("Escolha a opção desejada.\n");
+    printf(" Escolha a opcao desejada.\n");
     scanf("%d", &opcao_crono);
 }
 
 int avisos() {
-    system("cls");
-    printf("   Avisos\n");
-    printf(" ---------------\n");
-    printf("|    |\n");
-    printf("|  2. Mensal   |\n");
-    printf("|  0. Voltar   |\n");
-    printf(" ---------------\n\n");
+    printf("     Avisos\n");
+    printf("  ------------\n\n");
+    printf("  [3* ExpoUni InvistaC]  \n\n");
 
-    printf("Escolha a opção desejada.\n");
+    printf("  Pelo terceiro ano consecutivo, a InvestC promove  \n");
+    printf("  mais uma ExpoUni, uma exposicao online para os    \n");
+    printf("  alunos do ensino medio e universitarios de como o \n");
+    printf("  mercado financeiro opera, que acontecera nos dias \n");
+    printf("  05 e 08 de dezembro.                              \n");
+
+    printf(" -----------------------------------------------------\n\n");
+
+    printf("  [Atividades suspensas]  \n\n");
+
+    printf("  A equipe InvestC vem informar a suspensão de      \n");
+    printf("  atividades de atendimento ao cliente a partir do  \n");
+    printf("  dia 05 de dezembro. Demais funcoes do software    \n");
+    printf("  como informacoes para consulta  continuarao       \n");
+    printf("  funcionando.                                      \n\n");
+
+
+    printf(" Em caso de duvidas, consulte o cronograma da empresa.\n");
+    printf(" Pressione 0 para voltar.\n");
     scanf("%d", &opcao_crono);
+    break;
+
+    default:
+    printf("Comando invalido.\n");
 }
 
 //   Precisa:
 //   . ver como faz pra voltar pro menu CONSEGUIIII
 //   . criar função pro menu inicial e chamar CONSEGUIIIIII
-//   . colocar mais opçoes dentro das opcoes CONSEGUINDOOO
-//   . ver utf8
+//   . colocar mais opçoes dentro das opcoes CONSEGUINDO!!
+//   . ver utf8 NUM DEU, SÓ NA UNIP!!!!!!!!!!!!!!!
+
+//   . calendario no cronograma!!!!!!!!!!!!!!!!!!!
+//   . avisos (criar) CONSEGUIII
